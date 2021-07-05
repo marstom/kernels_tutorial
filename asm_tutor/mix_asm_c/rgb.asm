79ad15f012bd0cec27cd1da781d2bd68632cacdc
Tomasz Marszałek <marstomek@gmail.com>
2021-07-05 07:26:42 +0200

    step2 hex conv

--- asm_tutor/mix_asm_c/rgb.asm
+++ asm_tutor/mix_asm_c/rgb.asm
@@ -1,32 +1,40 @@
-
 global rgb
 
 section .text
 ;                 edi     esi    edx        rcx
 ; <----- int rgb(int r, int g, int b, char *outp) ----->
 rgb:
-    ; push rbp
-    ; mov rbp, rsp
-    mov r9, hex
 
-    ; push rdx
-    ; xor edx, edx
-    ; xor eax, eax
-    mov r9, 4
+division:
     xor rax, rax
     xor rdx, rdx
     mov rax, rdi
-    mov rdx, 16
-    div rax
-    ; pop rdx
+    ; loop1 (2)
+    mov rbx, 16
+    
+.two_hex_loop:
+    div rbx       ; rax / rbx    wynik w rax(al), reszta rdx (dl)
+    add dl, 48
+    mov byte [rcx+1],   dl
+    sub dl, 48
+    ;loop 2 (1)
+    mov rbx, 16
+    div rbx 
+    add dl, 48
+    mov byte [rcx],   dl
 
+    ; conv to digit
+    add al, 48
+    add dl, 48
 
+    ; mov byte [rcx],   dl
+    ; mov byte [rcx+1], 'e'
 
-    mov byte [rcx],   'H'
-    mov byte [rcx+1], 'e'
     mov byte [rcx+2], 'l'
     mov byte [rcx+3], 'l'
+    
     mov byte [rcx+4], 'o'
+    mov byte [rcx+4], 'x'
 
     mov rax, rcx
     ; rax <- outp
@@ -38,5 +46,3 @@ rgb:
 
 
 
-section .data
-    hex: db '0123456789ABCDEF'
