e2f4368bf2f380e13c192232ee5d105c886b8f52
Tomasz Marszałek <marstomek@gmail.com>
2021-07-17 08:41:48 +0200

    Now I can use at least GDB, dwarf is important!

--- asm_tutor/x64/stack.asm
+++ asm_tutor/x64/stack.asm
@@ -16,7 +16,7 @@ section .text
 
 main:
     lea rdi, [tablica]
-    mov rsi, 6
+    mov rsi, 5
     ; call divide
     call drukuj_tablice
 
@@ -50,21 +50,21 @@ drukuj_tablice: ; rdi  adres, rsi rozmiar
     
 .loop:
     cmp     rbx,  rsi
+    pushf   ; push flags on stack, bacause printf mess up my flags
     add     rbx, 1
-    jl      .cont
-    mov     dword [rdi+2*4], 77777   ; access element 3rd [2] of array and write 7777
+    jl      .cont                       ; continue when
+    mov     dword [rdi+2*4], 77777      ; access element 3rd [2] of array and write 7777
     push    rdi
     push    rsi
-    mov     rdi, msg    ; set 1st parameter (format)
-    ; mov     rsi, [tablica+4*(rbx-1)]  ; set 2nd parameter
+    mov     rdi, msg                    ; set 1st parameter (format)
     lea     r9, [tablica+4*(rbx-1)]     ; copy effective address to r9
     mov     rsi, [r9]                   ; get value from address under r9
-    ;lea     r9, [tablica+4*rsi]
-    xor     rax, rax    ; because printf is varargs
+    xor     rax, rax                    ; because printf is varargs
     call    printf
     pop     rsi
     pop     rdi
-    cmp     rbx, rsi
+    popf    ; to avoid cmp again I pushed flags
+    ; cmp rbx, rsi
     jle     .loop
 .cont:
     pop rbx
