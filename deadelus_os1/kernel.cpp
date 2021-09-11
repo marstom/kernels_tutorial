58c0948d573ce735eb2f1c242df2c1096375ae9d
Tomasz Marszałek <marstomek@gmail.com>
2021-09-11 09:36:38 +0200

    [deadeluskern] makefile create for it

--- deadelus_os1/kernel.cpp
+++ deadelus_os1/kernel.cpp
@@ -1,4 +1,10 @@
 extern "C" void main(){
-    *(char*)0xb8000 = 'Q';
+    char *video_mem = (char*)0xb8000;
+    // *(char*)0xb8000 = 'Q';
+    video_mem[0] = 'M';
+    video_mem[2] = 'y';
+    video_mem[4] = ' ';
+    video_mem[6] = 'f';
+    video_mem[6] = 'i';
     return;
 }
