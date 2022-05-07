9ebe148421a7cf3db5743d331c57683de5770e67
Tomasz Marszalek <tomasz.marszalek@sendcloud.com>
2022-05-07 07:20:37 +0200

    Dodałem od mojej wersji OS obsługe klawiatury!!

--- basedOnMellOs/Kernel/kernel_entry.asm
+++ basedOnMellOs/Kernel/kernel_entry.asm
@@ -1,16 +1,20 @@
 section .text
 [bits 32]
 
+
+
 global MemSize
     MemSize: db 0, 0
     mov [MemSize], bx   ; Get memory size from bx
 
+
+_start:
 [extern main]
 call main		        ; calls kernel function main()
-
 jmp $
 
 ;%include "../CPU/interrupt.asm"
+
 
 
 section .rodata		    ; read only data
